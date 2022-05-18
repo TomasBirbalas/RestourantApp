7f0a4b3a7b0953e23b113fca7df1f656697d3847
tomasbir <tomas.birbalas@gmail.com>
2022-05-18 21:33:34 +0300

    Get Data from CSV file

---
+++ Repository/DataAccess/ProductRepo.cs
@@ -0,0 +1,31 @@
+﻿using Repository.Models;
+using Repository.Models.OrderModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DataAccess
+{
+    public class ProductRepo
+    {
+        List<Product> retrevieData = new List<Product>();
+
+        public List<Product> RetrevieData()
+        {
+            var dataFile = File.ReadAllLines(@"D:\Programavimo kursai\NET\Advanced\RestourantApp\DataFiles\food-menu.csv")
+                .Skip(1)
+                .Select(x => x.Split(','))
+                .Select(x => new Food(
+                    x[0],
+                    x[1],
+                    Enum.Parse<Portion>(x[2]),
+                    decimal.Parse(x[3])
+                )).ToList();
+            retrevieData.AddRange(dataFile);
+            return retrevieData;
+        }
+    }
+}
