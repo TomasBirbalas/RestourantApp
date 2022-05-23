2024bdf227a1aa57052797028ff7cc2ca4d3f927
tomasbir <tomas.birbalas@gmail.com>
2022-05-23 20:26:11 +0300

    Created JSON Deserializer

---
+++ Repository/Deserializer/JSONdeserializer.cs
@@ -0,0 +1,28 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Repository.Deserializer
+{
+    public class JSONdeserializer
+    {
+        public List<Waitress> DeserializeWaitress(string filePath)
+        {
+            var jsonString = File.ReadAllText(filePath);
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+
+            var jsonData = JsonSerializer.Deserialize<List<Waitress>>(jsonString, options);
+
+            return jsonData;
+        }
+    }
+}
