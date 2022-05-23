a8519c51d5b5bd7109f60ad88d31a0e45f426711
tomasbir <tomas.birbalas@gmail.com>
2022-05-23 21:38:00 +0300

    Created Functions for Waitress

---
+++ BusinessLogic/Services/WaitressServices.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class Global
+    {
+        public static int id;
+        public static int pinCode;
+    }
+    public class WaitressServices
+    {
+        public Table GetNewTable(Waitress waitress, int personCount)
+        {
+            List<Table> tableList = waitress.TableList;
+
+            tableList = tableList.FindAll(table => table.Status == false).ToList();
+            return tableList.OrderBy(x => Math.Abs(x.NumberOfSeats - personCount)).First();
+        }
+    }
+
+}
