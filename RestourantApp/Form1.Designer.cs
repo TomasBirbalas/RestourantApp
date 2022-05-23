bca7adeb271c8c500cabfabf3573f3f2e04edf01
tomasbir <tomas.birbalas@gmail.com>
2022-05-23 21:38:56 +0300

    changed layout

--- RestourantApp/Form1.Designer.cs
+++ RestourantApp/Form1.Designer.cs
@@ -35,7 +35,9 @@
             this.inputUserId = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.panelHeader = new System.Windows.Forms.Panel();
+            this.textBoxFullName = new System.Windows.Forms.TextBox();
             this.panelMain.SuspendLayout();
+            this.panelHeader.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
@@ -96,11 +98,21 @@
             // 
             // panelHeader
             // 
+            this.panelHeader.Controls.Add(this.textBoxFullName);
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Name = "panelHeader";
             this.panelHeader.Size = new System.Drawing.Size(1008, 129);
             this.panelHeader.TabIndex = 1;
+            // 
+            // textBoxFullName
+            // 
+            this.textBoxFullName.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBoxFullName.Location = new System.Drawing.Point(773, 52);
+            this.textBoxFullName.Name = "textBoxFullName";
+            this.textBoxFullName.ReadOnly = true;
+            this.textBoxFullName.Size = new System.Drawing.Size(206, 23);
+            this.textBoxFullName.TabIndex = 0;
             // 
             // Form1
             // 
@@ -114,6 +126,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panelMain.ResumeLayout(false);
             this.panelMain.PerformLayout();
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -127,5 +141,6 @@
         private System.Windows.Forms.Label lbPinCode;
         private System.Windows.Forms.Label lbId;
         private System.Windows.Forms.TextBox inputUserPinCode;
+        private System.Windows.Forms.TextBox textBoxFullName;
     }
 }
