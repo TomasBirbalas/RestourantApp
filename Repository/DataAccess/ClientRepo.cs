c07daf07a216ed012cbf9f69d1f2ebdebbbb6e7c
tomasbir <tomas.birbalas@gmail.com>
2022-05-27 14:58:51 +0300

    created client repository

---
+++ Repository/DataAccess/ClientRepo.cs
@@ -0,0 +1,30 @@
+﻿using Repository.Deserializer;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DataAccess
+{
+    public class ClientRepo
+    {
+        private List<Client> clientList { get; }
+
+        public ClientRepo()
+        {
+            JSONdeserializer deserializer = new JSONdeserializer();
+            clientList = deserializer.DeserializeClient(@"..\..\..\..\DataFiles\clientJSON.json");
+        }
+
+        public List<Client> RetrieveClientList()
+        {
+            return clientList;
+        }
+        public Client RetrieveClient(int tableId)
+        {
+            return clientList.Find(client => client.TableID == tableId);
+        }
+    }
+}
