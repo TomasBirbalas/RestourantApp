5d885ffb392af1c5b2cb8172252d592825f62b42
tomasbir <tomas.birbalas@gmail.com>
2022-05-27 14:59:45 +0300

    Created JSON data serializer

---
+++ Repository/Serializer/DataSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Repository.Serializer
+{
+    public class DataSerializer
+    {
+        public void SerializeData<T>( string filePath, List<T> dataList)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+            if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            }
+            string jsonString = JsonSerializer.Serialize(dataList, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
